fa4e3a323a0a6f581fd80bc72e1c69dac1402808
hossamghoneim <hossamghoneim11@gmail.com>
2023-06-12 12:05:14 +0300

    changes

---
+++ src/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Webstdy\Notification;
+use Illuminate\Support\Facades\Route;
+
+/** notifications routes **/
+
+Route::post('/save-token', 'Webstdy\Notification\NotificationController@saveToken')->name('save-token');
+Route::get('notifications/{id}/mark_as_read', 'Webstdy\Notification\NotificationController@markAsRead')->name('notifications.mark_as_read');
+Route::get('notifications/{type}/load-more/{next}', 'Webstdy\Notification\NotificationController@loadMore')->name('notifications.load_more');
+Route::get('notifications/mark-all-as-read', 'Webstdy\Notification\NotificationController@markAllAsRead')->name('notifications.mark_all_as_read');
+Route::post('notifications/change-status-sound' , 'Webstdy\Notification\NotificationController@changeSoundStatus')->name('notifications.change-sound-status');
